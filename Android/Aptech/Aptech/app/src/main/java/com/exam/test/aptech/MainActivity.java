add4095038d335727f61bcbffc6ce399353161b1
王涛 <desl0451@126.com>
2017-04-23 15:41:16 +0800

    1
    1

---
+++ Android/Aptech/Aptech/app/src/main/java/com/exam/test/aptech/MainActivity.java
@@ -0,0 +1,14 @@
+package com.exam.test.aptech;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //设置界面显示linearlayout布局 (线性布局)
+        setContentView(R.layout.linearlayout);
+    }
+}
