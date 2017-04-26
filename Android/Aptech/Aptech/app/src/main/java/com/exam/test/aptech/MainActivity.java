5cd420a342514b3d1ba0b6a6b5f6e8cc832e1a8f
王涛 <desl0451@126.com>
2017-04-26 11:24:56 +0800

    20170426
    111

--- Android/Aptech/Aptech/app/src/main/java/com/exam/test/aptech/MainActivity.java
+++ Android/Aptech/Aptech/app/src/main/java/com/exam/test/aptech/MainActivity.java
@@ -8,7 +8,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //设置界面显示linearlayout布局 (线性布局)
-        setContentView(R.layout.linearlayout);
+        //示例5  设置界面显示linearlayout布局 (线性布局)
+        //setContentView(R.layout.linearlayout);
+        //示例7  设置界面显示relativelayout布局 (相对布局)
+        //setContentView(R.layout.relativelayout);
+        //示例8  设置界面显示框架布局 (相对布局)
+        //setContentView(R.layout.framelayout);
+        //上机练习3
+        //setContentView(R.layout.p293demo3);
+        //示例9  表格布局
+        //setContentView(R.layout.tablelayout);
+        //上机练习4
+        setContentView(R.layout.p294demo4);
     }
+
 }
